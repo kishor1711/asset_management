612f84aef3aa56c43331927f4bb2d95517654d67
Kishor Akolkar (NCS) <kishor.akolkar2@in.ncs-i.com>
2019-08-19 11:13:28 +0530

    First Commit of asset management front code

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
+
+import { BasicAuthInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';;
+import { AssetsManagementComponent } from './assets/assets-management/assets-management.component'
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent
+,
+        AssetsManagementComponent    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/assets/assets-management/assets-management.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assets-management',
+  templateUrl: './assets-management.component.html',
+  styleUrls: ['./assets-management.component.css']
+})
+export class AssetsManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // $('#myModal').modal({backdrop: 'static', keyboard: false})  ;
+  }
+
+}
